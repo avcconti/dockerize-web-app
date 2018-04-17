6698fc9e00473df7e92a17ecfc09e962cd22b4da
Andrés Velasco <avc.conti@gmail.com>
2018-04-17 16:48:31 +0200

    [+] New front design

--- 4-entrypoint/index.php
+++ 4-entrypoint/index.php
@@ -15,22 +15,42 @@ if(!$connect){
 ?>
 <!doctype html>
 <html class="no-js" lang="">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title></title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style>
+body {
+    background: #3498db;
+    font-family: sans-serif;
+}
+h1 {
+    position: relative;
+    color: rgba(0, 0, 0, .3);
+    font-size: 5em
+}
+h1:before {
+    content: attr(data-text);
+    position: absolute;
+    overflow: hidden;
+    max-width: 7em;
+    white-space: nowrap;
+    color: #fff;
+    animation: loading 8s linear;
+}
+@keyframes loading {
+    0% {
+        max-width: 0;
+    }
+}
+</style>
 
+    </head>
+    <body>
+        <h1 style="width:400px; margin:auto auto;" data-text="NGINX...">NGINX...</h1>
+        <h1 style="width:400px; margin:auto auto;" data-text="PHP <?php echo $redis->incr("counter")." with MY_ENV_VAR =>".getenv("MY_ENV_VAR");?>">PHP <?php echo $redis->incr("counter")." with MY_ENV_VAR =>".getenv("MY_ENV_VAR");?></h1>
 
-</head>
-<body>
-        <!--[if lte IE 9]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
-        <![endif]-->
-
-        <!-- Add your site or application content here -->
-        <p>Html:1</p>
-        <p>PHP:<?php echo $redis->incr("counter")." with MY_ENV_VAR =>".getenv("MY_ENV_VAR");?></p>
     </body>
-    </html>
+</html>
