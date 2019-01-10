8251403cb6f2207281fec07cc6610e6a790883d1
Andrés Velasco <avc.conti@gmail.com>
2019-01-10 14:31:28 +0100

    [+] sed moved to entrypoint from dockerfilePHP

--- 4-entrypoint/docker-entrypoint.sh
+++ 4-entrypoint/docker-entrypoint.sh
@@ -7,6 +7,12 @@ cat  << EOM
 💻 💻 💻 💻 💻 💻 💻                                      
 EOM
 
+sed -e 's/127.0.0.1:9000/9000/' \
+        -e '/allowed_clients/d' \
+        -e '/catch_workers_output/s/^;//' \
+        -e 's/;clear_env/clear_env/' \
+        -e '/error_log/d' \
+        -i /etc/php-fpm.d/www.conf 
 
 export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID:-1}"
 export AWS_SECRET_ACCESS_KEY=miau2
