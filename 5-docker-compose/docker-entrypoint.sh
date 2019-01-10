e8c1eb794ac550694dce812343f3c525993b46d8
Andrés Velasco <avc.conti@gmail.com>
2019-01-10 16:51:45 +0100

    [+] Excercise 5 adapted to env changes from 4

--- 5-docker-compose/docker-entrypoint.sh
+++ 5-docker-compose/docker-entrypoint.sh
@@ -7,4 +7,13 @@ cat  << EOM
 💻 💻 💻 💻 💻 💻 💻                                      
 EOM
 
+sed -e 's/127.0.0.1:9000/9000/' \
+        -e '/allowed_clients/d' \
+        -e '/catch_workers_output/s/^;//' \
+        -e 's/;clear_env/clear_env/' \
+        -e '/error_log/d' \
+        -i /etc/php-fpm.d/www.conf 
+
+export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID:-1}"
+
 exec "$@"
